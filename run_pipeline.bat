0deeddb55bee5a63eb503ddbfa38b590310df4f1
Rudrajit Bhattacharyya <rudrajitb24@gmail.com>
2025-12-19 23:36:05 +0530

    Fixed Task Scheduler Issues

--- run_pipeline.bat
+++ run_pipeline.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal
 
 REM ---- Go to project directory ----
 cd /d "C:\Users\Rudra\Documents\Retail Price Tracking"
@@ -8,3 +9,6 @@ call venv\Scripts\activate
 
 REM ---- Run pipeline and capture logs ----
 python main.py > pipeline_log.txt 2>&1
+
+REM ---- Explicitly exit cmd ----
+exit /b 0
